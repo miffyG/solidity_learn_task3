f3db0230976e2df80bd0cb6cac2d70d92ae29130
steven <154365966@qq.com>
2025-09-08 11:58:41 +0800

    add mock contract for testing

---
+++ contracts/mocks/MockPriceFeed.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract MockPriceFeed is AggregatorV3Interface {
+    uint8 private _decimals;
+    int256 private _price;
+    uint256 private _updatedAt;
+    uint80 private _roundId;
+
+    constructor(uint8 decimals_, int256 price_) {
+        _decimals = decimals_;
+        _price = price_;
+        _updatedAt = block.timestamp;
+        _roundId = 1;
+    }
+
+    function decimals() external view override returns (uint8) {
+        return _decimals;
+    }
+
+    function description() external pure override returns (string memory) {
+        return "Mock Price Feed";
+    }
+
+    function version() external pure override returns (uint256) {
+        return 1;
+    }
+
+    function getRoundData(uint80 roundId_) external view override returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound
+    ) {
+        return (roundId_, _price, _updatedAt, _updatedAt, _roundId);
+    }
+
+    function latestRoundData() external view override returns (
+        uint80 roundId,
+        int256 answer,
+        uint256 startedAt,
+        uint256 updatedAt,
+        uint80 answeredInRound
+    ) {
+        return (_roundId, _price, _updatedAt, _updatedAt, _roundId);
+    }
+
+    function setPrice(int256 price_) external {
+        _price = price_;
+        _updatedAt = block.timestamp;
+        _roundId++;
+    }
+}
